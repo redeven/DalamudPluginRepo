98859594b9e659170e0b31b5979f4675f68495f2
Redeven <18680184+redeven@users.noreply.github.com>
2023-01-23 08:17:19 -0300

    Generate initial repository

---
+++ generate-repository.js
@@ -0,0 +1,12 @@
+const { readdirSync, readFileSync, writeFileSync } = require('fs');
+const path = require('path');
+try {
+  const repositories = readdirSync(path.resolve(__dirname), { withFileTypes: true })
+    .filter((d) => d.isDirectory() && d.name !== '.git')
+    .map((d) => path.resolve(__dirname, d.name, 'repository.json'))
+    .map((repoPath) => readFileSync(repoPath, 'utf-8'))
+    .map((repoString) => JSON.parse(repoString));
+
+  const combinedRepository = [].concat(...repositories);
+  writeFileSync('repository.json', JSON.stringify(combinedRepository, null, 4), 'utf-8');
+} catch (e) {}
